66e4dbc5ce1fc27e2a68178293dec0cf1688ab29
Sami-T01 <samitahir018@gmail.com>
2026-03-29 21:11:53 +0100

    mobile background fix

--- components/main-shell-background.tsx
+++ components/main-shell-background.tsx
@@ -3,7 +3,7 @@ export function MainShellBackground() {
   return (
     <div
       aria-hidden
-      className="pointer-events-none fixed inset-0 z-0 min-h-[100dvh] bg-[url('/mobile-background.png')] bg-cover bg-center bg-no-repeat opacity-[0.72] md:bg-[url('/main-background.png')] md:bg-fixed"
+      className="pointer-events-none fixed top-[calc(-1*env(safe-area-inset-top,0px))] right-[calc(-1*env(safe-area-inset-right,0px))] bottom-[calc(-1*env(safe-area-inset-bottom,0px))] left-[calc(-1*env(safe-area-inset-left,0px))] z-0 bg-[url('/mobile-background.png')] bg-cover bg-center bg-no-repeat opacity-[0.72] md:inset-0 md:bg-[url('/main-background.png')] md:bg-fixed"
     />
   );
 }
